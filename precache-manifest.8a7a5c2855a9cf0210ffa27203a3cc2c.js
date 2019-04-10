bb546fb056b23ae8b645e12b0d652f6c740c8a38
ABuntsev <andrew.buntsev@gmail.com>
2019-04-10 15:34:28 +1000

    Updates

---
+++ precache-manifest.8a7a5c2855a9cf0210ffa27203a3cc2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  },
+  {
+    "revision": "a4008e243caa6bbcdb9f",
+    "url": "/drum-machine/static/js/main.f4f46a4e.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/drum-machine/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "a4008e243caa6bbcdb9f",
+    "url": "/drum-machine/static/css/main.f4589ba1.chunk.css"
+  },
+  {
+    "revision": "41dcbef1147ed1ab96b38847e426a6e1",
+    "url": "/drum-machine/index.html"
+  }
+];
